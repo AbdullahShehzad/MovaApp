3112b7951edeb7883117c31a4ab5f2fc639e694d
abdullahshehzad <abdullah.shehzad58155@gmail.com>
2024-07-11 16:36:38 +0500

    kotlin version updated

--- app/src/main/java/com/example/myapplication/HomeScreen.kt
+++ app/src/main/java/com/example/myapplication/HomeScreen.kt
@@ -7,6 +7,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
@@ -19,21 +20,30 @@ class HomeScreen : Fragment(R.layout.fragment_homescreen) {
     private val adapter1: CustomAdapter = CustomAdapter()
     private val adapter2: CustomAdapter = CustomAdapter()
 
-    val viewModel: HomeScreenViewModel = HomeScreenViewModel()
+    private val viewModel by viewModels<HomeScreenViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel.dataInit()
-
         addObservers()
+
         view.findViewById<RecyclerView?>(R.id.recyclerView1)?.apply {
-            setHasFixedSize(true)
             adapter = adapter1
+
+            //PAGINATION LOGIC
+            addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+                    if (!recyclerView.canScrollHorizontally(1)) {
+                        //Implement the new Api call here.
+                        viewModel.dataInit()
+                    }
+                }
+            })
         }
 
         view.findViewById<RecyclerView?>(R.id.recyclerView2)?.apply {
-            setHasFixedSize(true)
             adapter = adapter2
         }
 
@@ -44,7 +54,7 @@ class HomeScreen : Fragment(R.layout.fragment_homescreen) {
         }
     }
 
-    fun addObservers(){
+    private fun addObservers(){
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 launch {
